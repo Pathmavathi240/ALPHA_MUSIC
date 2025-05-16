4da30b294aab9466fd131656234b2dc57a3c3c8d
Pathmavathi240 <pathmavathi975146@gmail.com>
2025-05-16 20:24:00 +0530

    Update __main__.py

--- SONALI/__main__.py
+++ SONALI/__main__.py
@@ -1,16 +1,27 @@
+#
+# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
+#
+# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+
 import asyncio
 import importlib
+import sys
 
 from pyrogram import idle
 from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
-from SONALI import LOGGER, app, userbot
-from SONALI.core.call import Sona
-from SONALI.misc import sudo
-from SONALI.plugins import ALL_MODULES
-from SONALI.utils.database import get_banned_users, get_gbanned
 from config import BANNED_USERS
+from YukkiMusic import LOGGER, app, userbot
+from YukkiMusic.core.call import Yukki
+from YukkiMusic.plugins import ALL_MODULES
+from YukkiMusic.utils.database import get_banned_users, get_gbanned
+
+loop = asyncio.get_event_loop()
 
 
 async def init():
@@ -21,9 +32,17 @@ async def init():
         and not config.STRING4
         and not config.STRING5
     ):
-        LOGGER(__name__).error("Assistant client variables not defined, exiting...")
-        exit()
-    await sudo("7994196725")
+        LOGGER("YukkiMusic").error(
+            "No Assistant Clients Vars Defined!.. Exiting Process."
+        )
+        return
+    if (
+        not config.SPOTIFY_CLIENT_ID
+        and not config.SPOTIFY_CLIENT_SECRET
+    ):
+        LOGGER("YukkiMusic").warning(
+            "No Spotify Vars defined. Your bot won't be able to play spotify queries."
+        )
     try:
         users = await get_gbanned()
         for user_id in users:
@@ -35,28 +54,28 @@ async def init():
         pass
     await app.start()
     for all_module in ALL_MODULES:
-        importlib.import_module("SONALI.plugins" + all_module)
-    LOGGER("SONALI.plugins").info("Successfully Imported Modules...")
+        importlib.import_module("YukkiMusic.plugins" + all_module)
+    LOGGER("Yukkimusic.plugins").info(
+        "Successfully Imported Modules "
+    )
     await userbot.start()
-    await Sona.start()
+    await Yukki.start()
     try:
-        await Sona.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
-    except NoActiveGroupCall:
-        LOGGER("SONALI").error(
-            "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
+        await Yukki.stream_call(
+            "http://docs.evostream.com/sample_content/assets/sintel1m720p.mp4"
         )
-        exit()
+    except NoActiveGroupCall:
+        LOGGER("YukkiMusic").error(
+            "[ERROR] - \n\nPlease turn on your Logger Group's Voice Call. Make sure you never close/end voice call in your log group"
+        )
+        sys.exit()
     except:
         pass
-    await Sona.decorators()
-    LOGGER("SONALI").info(
-        "DROP YOUR GIRLFRIEND'S NUMBER AND SEXY PIC TO @ll_ALPHA_BABY_lll , @PURVI_SUPPORT FOR ANY ISSUES MADE BY SONALI"
-    )
+    await Yukki.decorators()
+    LOGGER("YukkiMusic").info("Yukki Music Bot Started Successfully")
     await idle()
-    await app.stop()
-    await userbot.stop()
-    LOGGER("SONALI").info("Stopping DEEP Music Bot...")
 
 
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(init(self))
+    loop.run_until_complete(init())
+    LOGGER("YukkiMusic").info("Stopping Yukki Music Bot! GoodBye")
